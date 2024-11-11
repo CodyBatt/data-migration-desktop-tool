4599af175d087e3a8d5d8e707c0355fb81eebc36
Cody Batt <cody.batt@oneidentity.com>
2024-11-11 16:09:19 -0700

    Add support for overwrite and batched requests

--- Extensions/AzureTableAPI/Cosmos.DataTransfer.AzureTableAPIExtension/AzureTableAPIDataSinkExtension.cs
+++ Extensions/AzureTableAPI/Cosmos.DataTransfer.AzureTableAPIExtension/AzureTableAPIDataSinkExtension.cs
@@ -20,17 +20,32 @@ namespace Cosmos.DataTransfer.AzureTableAPIExtension
 
             var serviceClient = new TableServiceClient(settings.ConnectionString);
             var tableClient = serviceClient.GetTableClient(settings.Table);
+            await tableClient.CreateIfNotExistsAsync(cancellationToken);
 
-            await tableClient.CreateIfNotExistsAsync();
-
-            var createTasks = new List<Task>();
+            var batchSize = settings.BatchSize;
+            int batchNumber = 1;
+            int entityTotal = 0;
+            var transactionActions = new List<TableTransactionAction>();
             await foreach(var item in dataItems.WithCancellation(cancellationToken))
             {
-               var entity = item.ToTableEntity(settings.PartitionKeyFieldName, settings.RowKeyFieldName);
-               createTasks.Add(tableClient.AddEntityAsync(entity));
+                var tableEntity = item.ToTableEntity(settings.PartitionKeyFieldName, settings.RowKeyFieldName);
+                transactionActions.Add(settings.Overwrite
+                    ? new TableTransactionAction(TableTransactionActionType.Add, tableEntity)
+                    : new TableTransactionAction(TableTransactionActionType.UpdateReplace, tableEntity));
+                entityTotal++;
+
+                if (transactionActions.Count == batchSize)
+                {
+                    Console.Write($"Processing batch: {batchNumber++} (Size: {batchSize})\r");
+                    await tableClient.SubmitTransactionAsync(transactionActions, cancellationToken);
+                    transactionActions = new List<TableTransactionAction>();
+                }
             }
 
-            await Task.WhenAll(createTasks);
+            // Process remainder
+            Console.WriteLine($"Processing remainder: {transactionActions.Count}                                                   ");
+            await tableClient.SubmitTransactionAsync(transactionActions, cancellationToken);
+            Console.WriteLine($"Entities processed: {entityTotal}");
         }
 
         public IEnumerable<IDataExtensionSettings> GetSettings()
--- Extensions/AzureTableAPI/Cosmos.DataTransfer.AzureTableAPIExtension/Settings/AzureTableAPIDataSinkSettings.cs
+++ Extensions/AzureTableAPI/Cosmos.DataTransfer.AzureTableAPIExtension/Settings/AzureTableAPIDataSinkSettings.cs
@@ -2,5 +2,14 @@
 {
     public class AzureTableAPIDataSinkSettings : AzureTableAPISettingsBase
     {
+        /// <summary>
+        /// Indicates whether to overwrite existing entries
+        /// </summary>
+        public bool Overwrite { get; set; }
+
+        /// <summary>
+        /// Size of transaction batch
+        /// </summary>
+        public int BatchSize { get; set; } = 100;
     }
 }
